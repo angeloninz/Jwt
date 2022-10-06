c570c4c2e33f0d1f810061abd63f18d1b95e3595
angserrano <angelo.g.serrano@gmail.com>
2022-10-06 16:05:32 +0800

    Add project files.

---
+++ Jwt/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Jwt
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
